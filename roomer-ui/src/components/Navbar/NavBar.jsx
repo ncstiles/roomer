0696be99834bdb24dfee1c00c112b5aa0bd1d4c0
ncstiles <ncstiles17@gmail.com>
2022-07-22 12:05:25 -0700

    Modified navbar to contain link to the profile view

--- roomer-ui/src/components/Navbar/NavBar.jsx
+++ roomer-ui/src/components/Navbar/NavBar.jsx
@@ -2,21 +2,13 @@ import * as React from "react";
 import "./NavBar.css";
 import { useNavigate } from "react-router-dom";
 import { BASE_API_URL } from "../../constants";
-import axios from "axios";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { Dropdown } from "react-bootstrap";
+import axios from "axios";
+import { Button } from "react-bootstrap";
 
-export default function NavBar({ setIsLoggedIn }) {
+export default function NavBar({ setIsLoggedIn, username }) {
   const nav = useNavigate();
-
-  const logout = () => {
-    axios({
-      method: "get",
-      url: BASE_API_URL + "/logout",
-    })
-      .then(() => {
-        setIsLoggedIn(false);
-      })
-  };
 
   return (
     <>
@@ -28,7 +20,7 @@ export default function NavBar({ setIsLoggedIn }) {
           <span className="link" onClick={() => nav("/matches")}>
             MATCHES
           </span>
-          <span className="link">RECENTS</span>
+          <span className="link">LIKED</span>
           <span className="link">GROUPS</span>
         </div>
         {/*spacer to add distance between left/right sides of navbar */}
@@ -40,10 +32,31 @@ export default function NavBar({ setIsLoggedIn }) {
           <span className="link" onClick={() => nav("/register")}>
             REGISTER
           </span>
-          <span className="link" onClick={logout}>
+          <span className="link" onClick={() => nav("/logout")}>
             LOGOUT
           </span>
-          <AccountCircleIcon className="link navbar-pfp" fontSize="large" />
+
+          {username === "" ? null : (
+            <Button
+              className="link navbar-pfp"
+              variant="secondary"
+              onClick={() => nav("/profile")}
+            >
+              {username}
+            </Button>
+          )}
+
+          {/* <AccountCircleIcon className="link navbar-pfp" fontSize="large" onClick={()=> nav('/profile')}/> */}
+
+          {/* <Dropdown variant='secondary' className='dropdown'>
+            <Dropdown.Toggle variant='secondary' className="dropdown-toggle">
+              NICOLE
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/matches">Logout</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">View Profile</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>   */}
         </div>
       </nav>
     </>
